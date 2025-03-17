3726188b1f6749a2ebad219adaf33b52a7164448
Nethmika-LK <nethumd65@gmail.com>
2025-03-17 21:49:30 +0530

    Create owner.js

---
+++ owner.js
@@ -0,0 +1,11 @@
+{
+
+"footer":"> ",
+
+"imageurl":"https://files.catbox.moe/j4lyrn.jpeg",
+
+"news":"https://vajira-official-api.vercel.app/news/",
+
+"pair":"https://pair-v2-4bb5d6543a33.herokuapp.com/code?number="
+
+}
